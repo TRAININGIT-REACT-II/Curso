30574d41eb064fe3288ac19884e2416639f30db8
Angel M <hi@angel.kiwi>
2021-04-27 21:42:30 +0200

    Update tests on 7.1

--- ejemplos/7.1/__tests__/async.spec.js
+++ ejemplos/7.1/__tests__/async.spec.js
@@ -31,28 +31,3 @@ describe("promise", () => {
     return expect(Promise.reject("error")).rejects.toBe("error");
   });
 });
-
-describe("async / await", () => {
-  it("resuelve haciendo uso de async / await", async () => {
-    const result = await Promise.resolve("test");
-    expect(result).toBe("test");
-  });
-
-  it("cancela haciendo uso de async / await", async () => {
-    expect.assertions(1);
-
-    try {
-      const result = await Promise.reject("error");
-    } catch (err) {
-      expect(err).toMatch("error");
-    }
-  });
-
-  it("resuelve haciendo uso de async / await y el metodo resolve", async () => {
-    await expect(Promise.resolve("test")).resolves.toBe("test");
-  });
-
-  it("cancela haciendo uso de async / await y el metodo expect", async () => {
-    await expect(Promise.reject("error")).rejects.toBe("error");
-  });
-});
